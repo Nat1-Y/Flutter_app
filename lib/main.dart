62a5eac08c35a45a194f2d9421980ac57b289667
Natnael Yohannes <163080443+Nat1-Y@users.noreply.github.com>
2026-07-17 13:32:46 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ void main() {
             //background color
             backgroundColor: Colors.blueGrey[900],// background colour
           ),
+          //center
           body: Center(//image
             child: const Image (
                 image: NetworkImage('https://cdn.quotesgram.com/small/99/46/1398009570-diamond-sparkle_2_.jpg'),
